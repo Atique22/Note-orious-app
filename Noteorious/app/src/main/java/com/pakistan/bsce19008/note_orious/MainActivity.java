cccee0a0f1704955fb0364c3c542261950b56c62
Atique Ahmad <65412984+Atique22@users.noreply.github.com>
2023-02-24 18:24:14 +0500

    Application createD

---
+++ Noteorious/app/src/main/java/com/pakistan/bsce19008/note_orious/MainActivity.java
@@ -0,0 +1,26 @@
+package com.pakistan.bsce19008.note_orious;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LinearLayout scrollLayout = findViewById(R.id.linearlayout);
+        Button historyText;
+        historyText = new Button(getApplicationContext());
+        scrollLayout.addView(historyText);
+//        scrollLayout.addView(historyText);
+//        scrollLayout.addView(historyText);
+
+
+    }
+}
