3d72d84448c1236f7c98cac2d2e348c637a72dd8
Atique Ahmad <65412984+Atique22@users.noreply.github.com>
2023-02-27 13:58:55 +0500

    added

--- Noteorious/app/src/main/java/com/pakistan/bsce19008/note_orious/MainActivity.java
+++ Noteorious/app/src/main/java/com/pakistan/bsce19008/note_orious/MainActivity.java
@@ -1,17 +1,27 @@
 package com.pakistan.bsce19008.note_orious;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,16 +30,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LinearLayout scrollLayout = findViewById(R.id.linearlayout);
-        Button [] historyText = new Button[30];
         ImageButton addNoteBtn;
-        for (int i = 0; i < 30; i++) {
-            Button button = new Button(getApplicationContext());
-            button.setText("Button " + i);
-            historyText[i] = button;
-            scrollLayout.addView(historyText[i]);
+
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File directory = contextWrapper.getFilesDir();
+        File[] files = directory.listFiles();
+        Button [] historyText = new Button[files.length];
+        for (File file : files) {
+            int i=0;
+            if (file.isFile()) {
+                // Do something with the file, such as displaying its name on the screen
+                String fileName = file.getName();
+                Button button = new Button(getApplicationContext());
+                Log.d("File Name", fileName);
+                // Assume editingText is the reference to the AppCompatEditText object
+//                AppCompatEditText editingText = findViewById(R.id.editingText);
+
+                button.setText(fileName);
+                historyText[i] = button;
+                scrollLayout.addView(historyText[i]);
+                i++;
+            }
         }
-//        scrollLayout.addView(historyText);
-//        scrollLayout.addView(historyText);
+
+//        Button [] historyText = new Button[30];
+//        for (int i = 0; i < 30; i++) {
+//            Button button = new Button(getApplicationContext());
+//            button.setText("Button " + i);
+//            historyText[i] = button;
+//            scrollLayout.addView(historyText[i]);
+//        }
         addNoteBtn = findViewById(R.id.createBtn);
         addNoteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- Noteorious/app/src/main/java/com/pakistan/bsce19008/note_orious/MainActivityEditing.java
+++ Noteorious/app/src/main/java/com/pakistan/bsce19008/note_orious/MainActivityEditing.java
@@ -2,10 +2,20 @@ package com.pakistan.bsce19008.note_orious;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 
 public class MainActivityEditing extends AppCompatActivity {
 
@@ -14,8 +24,42 @@ public class MainActivityEditing extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_editing);
         Button back,save;
+        EditText messageText;// The text you want to save
+        messageText = findViewById(R.id.editingText);
         back = findViewById(R.id.backBtn);
         save = findViewById(R.id.saveBtn);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String fileName = "myFile_" + System.currentTimeMillis() + ".txt"; // Generate a unique file name based on the current time
+                        // Create a new file and write the text to it
+                        try {
+                            //internal storage
+                            String data = new String(messageText.toString());
+                            File dir = getFilesDir();
+                            File createFile = new File(dir, fileName);
+                            FileWriter fw = new FileWriter(createFile);
+                            fw.write(data);
+                            fw.close();
+                            // Show a  message to indicate that the file was saved successfully
+                            Log.d("************************", "save: created file successfully*****************: "+fileName);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
+
+            }
+        });
+
+
+
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +68,6 @@ public class MainActivityEditing extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
     }
 }
